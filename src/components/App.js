89f92446bd27f654fb0e9aa7cbfbc57238cf9074
jooooonis <xowns3213@naver.com>
2022-12-12 13:41:33 +0900

    feat : routers

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import firebase from '../firebase';
+import Home from '../pages/Home';
+import About from '../pages/About';
+
+function App() {
+  console.log(firebase);
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
