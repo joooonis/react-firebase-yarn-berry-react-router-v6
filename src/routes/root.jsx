40349b893ee01f95069ca776cd168d538d14ff73
jooooonis <xowns3213@naver.com>
2022-12-12 14:48:59 +0900

    feat : creating contacts Form component를 사용합니다.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,19 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLoaderData, Form } from 'react-router-dom';
+import { getContacts, createContact } from '../contacts';
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 export default function Root() {
+  const { contacts } = useLoaderData();
+
   return (
     <>
       <div id='sidebar'>
@@ -17,19 +30,33 @@ export default function Root() {
             <div id='search-spinner' aria-hidden hidden={true} />
             <div className='sr-only' aria-live='polite'></div>
           </form>
-          <form method='post'>
+          <Form method='post'>
             <button type='submit'>New</button>
-          </form>
+          </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{' '}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id='detail'>
