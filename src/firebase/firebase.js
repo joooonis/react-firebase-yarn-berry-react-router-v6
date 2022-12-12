0a8589e289ef6217384ee3196fd4d1555a6c732f
jooooonis <xowns3213@naver.com>
2022-12-12 14:22:24 +0900

    feat : firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.APP_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+const firebase = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const Login = (auth, email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+};
+
+export default firebase;
