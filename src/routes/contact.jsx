37c6b0679b1d2ce003830bb447690acc229e66fd
jooooonis <xowns3213@naver.com>
2022-12-12 15:11:24 +0900

    feat : update contacts with formdata

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -22,7 +22,7 @@ export default function Contact() {
             </>
           ) : (
             <i>No Name</i>
-          )}{' '}
+          )}
           <Favorite contact={contact} />
         </h1>
 
